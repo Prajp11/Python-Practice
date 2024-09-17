e884c6557ff74c7061c870e55f8da08562d12c68
Prajwal Pandgale <pandgale.prajwal11@gmail.com>
2024-09-17 20:20:10 +0530

    Started with String-Methods

---
+++ String-Methods/Capitalize.py
@@ -0,0 +1,11 @@
+# Original string
+name = 'prajwal'
+
+# Print the original string
+print("Original String:", name)
+
+# Apply capitalize method
+capitalized_name = name.capitalize()
+
+# Print the capitalized string
+print("Capitalized String:", capitalized_name)
