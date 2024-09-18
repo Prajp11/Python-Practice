6dd4f2e6b2486168db3c2f6bda7847113c27b6eb
Prajwal Pandgale <pandgale.prajwal11@gmail.com>
2024-09-18 11:10:38 +0530

    Started with String-Methods

---
+++ String-Methods/Casefold.py
@@ -0,0 +1,10 @@
+# Casefold()
+# Casefold() like the lower() oprater convert the upper case in the lower case
+
+
+Prajwal = 'PRAJWAL IS AN ENGINEER'
+print(Prajwal)
+
+
+Casefold_Method = Prajwal.casefold()
+print(Casefold_Method)
