2b08ad9550049189271d6608de97ca96820d188a
Prajwal Pandgale <pandgale.prajwal11@gmail.com>
2024-11-22 18:50:19 +0530

    Added .format() Method

---
+++ String-Methods/format.py
@@ -0,0 +1,7 @@
+# 9.format()
+# - Allows advanced string formatting using placeholders.
+# - Supports positional and keyword arguments.
+
+
+Str1 = 'The multiplaction of 11 * 11 is {0}'.format(11*11)
+print(Str1)
